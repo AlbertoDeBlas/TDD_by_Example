901a6f83b6d5a11261c0258c9ab57bf5c5fcd0c2
Alberto de Blas <alberto.de.blas@gmail.com>
2020-06-04 20:03:23 +0200

    Complete chapter 5
    feat: Complete chapter 5, add Franc class

--- src/test/java/TDD_tests.java
+++ src/test/java/TDD_tests.java
@@ -11,6 +11,12 @@ public class TDD_tests {
         assertEquals(new Dollar(15),five.times(3));
     }
 
+    public void testFrancMultiplication(){
+        Franc five = new Franc(5);
+        assertEquals(new Franc(10),five.times(2));
+        assertEquals(new Franc(15),five.times(3));
+    }
+
     @Test
     public void testEquality(){
         assertTrue(new Dollar(5).equals(new Dollar(5)));
