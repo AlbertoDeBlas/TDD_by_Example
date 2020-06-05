06251114c5ac437e782efbca17b5ec6d638d77c0
Alberto de Blas <alberto.de.blas@gmail.com>
2020-06-05 22:09:37 +0200

    Complete chapter 12
    feat: Complete chapter 12, add plus test and method, Bank class and Expression interface

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -1,22 +1,26 @@
-public class Money {
+public class Money implements Expression{
     protected int amount;
     protected String currency;
 
-    Money times(int multiplier){
+    Money times(int multiplier) {
         return new Money(amount * multiplier, currency);
     }
 
-    Money(int amount, String currency){
+    Money(int amount, String currency) {
         this.amount = amount;
         this.currency = currency;
     }
 
-    static Money dollar(int amount){
+    static Money dollar(int amount) {
         return new Money(amount, "USD");
     }
 
-    static Money franc(int amount){
+    static Money franc(int amount) {
         return new Money(amount,"CHF");
+    }
+
+    String currency() {
+        return currency;
     }
 
     public boolean equals(Object object) {
@@ -25,11 +29,11 @@ public class Money {
                 && currency().equals(money.currency());
     }
 
-    String currency(){
-        return currency;
+    Expression plus(Money addend) {
+        return new Money(amount + addend.amount, currency);
     }
 
-    public String toString(){
+    public String toString() {
         return amount + " "+ currency;
     }
 
--- src/test/java/TDD_tests.java
+++ src/test/java/TDD_tests.java
@@ -5,14 +5,14 @@ import static org.junit.jupiter.api.Assertions.*;
 public class TDD_tests {
 
     @Test
-    public void testMultiplication(){
+    public void testMultiplication() {
         Money five = Money.dollar(5);
         assertEquals(Money.dollar(10), five.times(2));
         assertEquals(Money.dollar(15),five.times(3));
     }
 
     @Test
-    public void testEquality(){
+    public void testEquality() {
         assertTrue(Money.dollar(5).equals(Money.dollar(5)));
         assertFalse(Money.dollar(5).equals(Money.dollar(6)));
         assertFalse(Money.franc(5).equals(Money.dollar(5)));
@@ -22,6 +22,15 @@ public class TDD_tests {
     public void testCurrency() {
         assertEquals("USD", Money.dollar(1).currency());
         assertEquals("CHF", Money.franc(1).currency());
+    }
+
+    @Test
+    public void testSimpleAddition() {
+        Money five = Money.dollar(5);
+        Expression sum = five.plus(five);
+        Bank bank = new Bank();
+        Money reduced = bank.reduce(sum, "USD");
+        assertEquals(Money.dollar(10), reduced);
     }
 }
 
