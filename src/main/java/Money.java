420eab21ab0c7c40e491fb696bf6c2e2b45bca8b
Alberto de Blas <alberto.de.blas@gmail.com>
2020-06-05 20:39:24 +0200

    Complete chapter 10
    feat: Complete chapter 10, move to Money times method from Dollar and Franc.

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -1,16 +1,14 @@
-public abstract class Money {
+public class Money {
     protected int amount;
     protected String currency;
 
-    abstract Money times(int multiplier);
+    Money times(int multiplier){
+        return new Money(amount * multiplier, currency);
+    };
 
     Money(int amount, String currency){
         this.amount = amount;
         this.currency = currency;
-    }
-
-    String currency(){
-        return currency;
     }
 
     static Dollar dollar(int amount){
@@ -24,7 +22,15 @@ public abstract class Money {
     public boolean equals(Object object) {
         Money money = (Money) object;
         return amount == money.amount
-                && getClass().equals(money.getClass());
+                && currency().equals(money.currency());
+    }
+
+    String currency(){
+        return currency;
+    }
+
+    public String toString(){
+        return amount + " "+ currency;
     }
 
 }
