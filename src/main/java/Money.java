99d240d5073ba1ee0d8011096e0c03fdadc7a8f2
Alberto de Blas <alberto.de.blas@gmail.com>
2020-06-06 23:25:07 +0200

    Complete chapter 15
    feat: Complete chapter 15, create test to sum mixed concurrencies, generalize Money for Expression

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -2,7 +2,7 @@ public class Money implements Expression{
     protected int amount;
     protected String currency;
 
-    Money times(int multiplier) {
+    Expression times(int multiplier) {
         return new Money(amount * multiplier, currency);
     }
 
@@ -29,7 +29,7 @@ public class Money implements Expression{
                 && currency().equals(money.currency());
     }
 
-    Expression plus(Money addend) {
+    public Expression plus(Expression addend) {
         return new Sum(this, addend);
     }
 
